174a1b0fba4934ea0352e4e34d79da1e0411bf62
sansay500 <borodachev@gmail.com>
2022-07-26 10:18:45 +0300

    Readme

--- back/resources/views/welcome.blade.php
+++ back/resources/views/welcome.blade.php
@@ -10,6 +10,7 @@ API calls (add to subversum.space/api):<br>
 /res_map<br>
 /buildings_map<br>
 /workers_map<br>
+Token Bearer: 1|vGbDvrBRev10rzQXJnoMCRLrjRPakbrqa62cXnkO
 <br>
 Database structure <br>
 <img width="70%" height="90%" src='https://user-images.githubusercontent.com/30046232/180787434-90a18ed3-5cb8-4389-93b4-c2f018bcee38.png'></img>
