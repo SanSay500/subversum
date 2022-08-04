e904d3085d7c141dcf776a08c5501ed69391db65
MarkZ04 <mark.ziiautdinov@gmail.com>
2022-08-04 18:03:48 +0300

    update

--- front/src/App.js
+++ front/src/App.js
@@ -1,111 +1,128 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
+
 const appStyle = {
-    height: '250px',
-    display: 'flex'
+	height: '250px',
+	display: 'flex',
 };
 
 const formStyle = {
-    margin: 'auto',
-    padding: '10px',
-    border: '1px solid #c9c9c9',
-    borderRadius: '5px',
-    background: '#f5f5f5',
-    width: '220px',
-    display: 'block'
+	margin: 'auto',
+	padding: '10px',
+	border: '1px solid #c9c9c9',
+	borderRadius: '5px',
+	background: '#f5f5f5',
+	width: '220px',
+	display: 'block',
 };
 
 const labelStyle = {
-    margin: '10px 0 5px 0',
-    fontFamily: 'Arial, Helvetica, sans-serif',
-    fontSize: '15px',
+	margin: '10px 0 5px 0',
+	fontFamily: 'Arial, Helvetica, sans-serif',
+	fontSize: '15px',
 };
 
 const inputStyle = {
-    margin: '5px 0 10px 0',
-    padding: '5px',
-    border: '1px solid #bfbfbf',
-    borderRadius: '3px',
-    boxSizing: 'border-box',
-    width: '100%'
+	margin: '5px 0 10px 0',
+	padding: '5px',
+	border: '1px solid #bfbfbf',
+	borderRadius: '3px',
+	boxSizing: 'border-box',
+	width: '100%',
 };
 
 const submitStyle = {
-    margin: '10px 0 0 0',
-    padding: '7px 10px',
-    border: '1px solid #efffff',
-    borderRadius: '3px',
-    background: '#3085d6',
-    width: '100%',
-    fontSize: '15px',
-    color: 'white',
-    display: 'block'
+	margin: '10px 0 0 0',
+	padding: '7px 10px',
+	border: '1px solid #efffff',
+	borderRadius: '3px',
+	background: '#3085d6',
+	width: '100%',
+	fontSize: '15px',
+	color: 'white',
+	display: 'block',
 };
 
-const Field = React.forwardRef(({label, type}, ref) => {
-    return (
-        <div>
-            <label style={labelStyle} >{label}</label>
-            <input ref={ref} type={type} style={inputStyle} />
-        </div>
-    );
+const Field = React.forwardRef(({ label, type }, ref) => {
+	return (
+		<div>
+			<label style={labelStyle}>{label}</label>
+			<input ref={ref} type={type} style={inputStyle} />
+		</div>
+	);
 });
 
-const Form = ({onSubmit}) => {
-    const usernameRef = React.useRef();
-    const passwordRef = React.useRef();
+const Form = ({ onSubmit }) => {
+	const usernameRef = React.useRef();
+	const passwordRef = React.useRef();
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        const data = {
-            email: usernameRef.current.value,
-            password: passwordRef.current.value
-        };
-        onSubmit(data);
-    };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const data = {
+			email: usernameRef.current.value,
+			password: passwordRef.current.value,
+		};
+		onSubmit(data);
+	};
 
-    return (
-        <form style={formStyle} onSubmit={handleSubmit} >
-            <Field ref={usernameRef} label="Email:" type="text" />
-            <Field ref={passwordRef} label="Password:" type="password" />
-            <div>
-                <button style={submitStyle} type="submit">Submit</button>
-            </div>
-        </form>
-    );
+	return (
+		<form style={formStyle} onSubmit={handleSubmit}>
+			<Field ref={usernameRef} label='Email:' type='text' />
+			<Field ref={passwordRef} label='Password:' type='password' />
+			<div>
+				<button style={submitStyle} type='submit'>
+					Submit
+				</button>
+			</div>
+		</form>
+	);
 };
 
 // Usage example:
 
 const App = () => {
-    const handleSubmit = data => {
-        const json = JSON.stringify(data, null, 4);
-        console.clear();
-        console.log(json);
-        fetch('https://subversum.space/api/login', {
-            method: 'POST',
-            body: json,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(response=>console.log(response.json()));
-        console.log('sent')
+	const [state, setState] = useState();
 
-    };
-    async function buildings()
-    {
-       fetch('https://subversum.space/api/buildings')
-           .then(response => console.log(response.json()));
-    }
-    return (
-        <>
-        <div style={appStyle}>
-            <Form onSubmit={handleSubmit} />
-        </div>
-            <button style={submitStyle} onClick={buildings}>Buildings</button>
-       </> 
-    );
+	const handleSubmit = (data) => {
+		const json = JSON.stringify(data, null, 4);
+
+		console.clear();
+		console.log(json);
+		fetch('https://subversum.space/api/login', {
+			method: 'POST',
+			body: json,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then((response) => console.log(response.json()));
+		console.log('sent');
+	};
+
+	function buildings() {
+		fetch('https://subversum.space/api/buildings')
+			.then((response) => response.json())
+			.then((d) => {
+				setState(d.data);
+			});
+	}
+
+	return (
+		<>
+			<div style={appStyle}>
+				<Form onSubmit={handleSubmit} />
+			</div>
+			<button style={submitStyle} onClick={buildings}>
+				Buildings
+			</button>
+
+			{state === undefined ? (
+				<></>
+			) : (
+				state.map((el, i) => <div key={i}>{el.type}</div>)
+			)}
+		</>
+	);
 };
 
 export default App;
