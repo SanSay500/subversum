006f50644feb1f716497ed7da2708f7991a0c1e1
sansay500 <borodachev@gmail.com>
2022-08-22 17:48:56 +0300

    last

--- front/src/index.js
+++ front/src/index.js
@@ -4,10 +4,18 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+document.title='Subversum Space';
+
 root.render(
     <React.StrictMode>
         <div className='container'>
-     <img src=  "image_2022-07-18_12-20-57.png"/>
+            <img src=  "image_2022-07-18_12-20-57.png"/>
+            <iframe width="1000" height="720" src="https://www.youtube.com/embed/hAUUmHvxqtA"
+                    title="SUBVERSUM Pre-Launch Trailer" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen>
+            </iframe>
+
             <p>COMING SOON...</p>
         {/*<App />*/}
         </div>
