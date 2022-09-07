db6f7bf9074ea59bd878429f144a9c7b6b4a1824
MarkZ04 <mark.ziiautdinov@gmail.com>
2022-09-07 17:56:43 +0300

    update indexAuth.js

--- front/js/indexAuth.js
+++ front/js/indexAuth.js
@@ -2,11 +2,15 @@ const btnName = document.getElementById('name');
 const btnRegister = document.getElementById('register');
 const btnLogin = document.getElementById('login');
 const btnLogout = document.getElementById('logout');
+
 btnName.style.display = 'none';
+btnLogout.style.display = 'none';
 
 if (window.sessionStorage.id) {
 	btnRegister.style.display = 'none';
 	btnLogin.style.display = 'none';
+
+	btnLogout.style.display = 'block';
 	btnName.style.display = 'block';
 	btnName.children[0].innerHTML = window.sessionStorage.name.toUpperCase();
 }
@@ -14,13 +18,13 @@ if (window.sessionStorage.id) {
 btnLogout.addEventListener('click', () => {
 	window.sessionStorage.clear();
 	const token = btoa('subversuman:mW5ihMGs');
-	axios
-		('/api/logout', {
-			method: 'POST',
-			headers: {
-				'Authorization': 'Basic ' + token,
-			},
-		})
+
+	axios('/api/logout', {
+		method: 'POST',
+		headers: {
+			Authorization: 'Basic ' + token,
+		},
+	})
 		.then(function (res) {
 			window.location.reload();
 		})
